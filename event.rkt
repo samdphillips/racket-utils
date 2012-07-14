849aa8963a1f6bc755af2169334a806dbb84b52a
Sam Phillips <samdphillips@gmail.com>
2012-07-14 23:51:05 +0000

    Converted to use test-suites

--- event.rkt
+++ event.rkt
@@ -2,7 +2,7 @@
 
 (require racket/function)
 
-(module+ test
+(module+ test-suite
   (require rackunit)
 
   (define (make-tracer)
@@ -43,15 +43,16 @@
       (const evt)))
   evt)
 
-(module+ test
-  (test-case "check timeout-evt"
-    (define a (timeout-evt 2000))
-    (check-false (sync/timeout 0 (wrap-evt a (const 'oops)))
+(module+ test-suite
+  (define-test-suite check-timeout-evt
+    (test-case "check timeout-evt"
+      (define a (timeout-evt 2000))
+      (check-false (sync/timeout 0 (wrap-evt a (const 'oops)))
+                   "timeout-evt is ready after timer expires")
+      (check-eq? (sync/timeout 4 (wrap-evt a (const 'ok))) 'ok
                  "timeout-evt is ready after timer expires")
-    (check-eq? (sync/timeout 4 (wrap-evt a (const 'ok))) 'ok
-               "timeout-evt is ready after timer expires")
-    (check-eq? (sync/timeout 0 (wrap-evt a (const 'ok))) 'ok
-               "timeout-evt doesn't reset on sync")))
+      (check-eq? (sync/timeout 0 (wrap-evt a (const 'ok))) 'ok
+                 "timeout-evt doesn't reset on sync"))))
 
 (define (timer-evt msecs)
   (define evt
@@ -63,15 +64,16 @@
           (const evt)))))
   evt)
 
-(module+ test
-  (test-case "check timer-evt"
-    (define a (timer-evt 2000))
-    (check-false (sync/timeout 0 (wrap-evt a (const #t)))
-                 "timer-evt is ready after timer expires")
-    (check-eq? (sync/timeout 4 (wrap-evt a (const 'ok))) 'ok)
-    (check-false (sync/timeout 0 (wrap-evt a (const #t)))
-                 "timer-evt resets on each sync")
-    (check-eq? (sync/timeout 4 (wrap-evt a (const 'ok))) 'ok)))
+(module+ test-suite
+  (define-test-suite check-timer-evt
+    (test-case "check timer-evt"
+      (define a (timer-evt 2000))
+      (check-false (sync/timeout 0 (wrap-evt a (const #t)))
+                   "timer-evt is ready after timer expires")
+      (check-eq? (sync/timeout 4 (wrap-evt a (const 'ok))) 'ok)
+      (check-false (sync/timeout 0 (wrap-evt a (const #t)))
+                   "timer-evt resets on each sync")
+      (check-eq? (sync/timeout 4 (wrap-evt a (const 'ok))) 'ok))))
 
 (define (poll-evt poll-thunk
                   #:interval [interval-thunk (const 1000)]
@@ -104,51 +106,60 @@
           (thread poller)
           ch))))))
 
-(module+ test
-  (test-case "poll-evt calls cleanup thunk if not chosen"
-    (define-values (trace get) (make-tracer))
+(module+ test-suite
+  (define-test-suite check-poll-evt
+    (test-case "poll-evt calls cleanup thunk if not chosen"
+      (define-values (trace get) (make-tracer))
+  
+      (sync (wrap-evt (timer-evt 7000)
+                      (thunk* (trace 'finished)))
+            (poll-evt (thunk (trace 'polling) #f)
+                      #:interval
+                      (const 2000)
+                      #:cleanup
+                      (thunk (trace 'cancelling))))
+      
+      (sleep 0)
+      (check-equal? (get)
+                    '((2 polling) (4 polling) (6 polling)
+                      (7 finished) (7 cancelling))))
 
-    (sync (wrap-evt (timer-evt 7000)
-                    (thunk* (trace 'finished)))
-          (poll-evt (thunk (trace 'polling) #f)
-                    #:interval
-                    (const 2000)
-                    #:cleanup
-                    (thunk (trace 'cancelling))))
+      (test-case "poll-evt is ready with the value of  poll when it returns non-false"
+        (define-values (trace get) (make-tracer))
+        (define count 2)
     
-    (sleep 0)
-    (check-equal? (get)
-                  '((2 polling) (4 polling) (6 polling)
-                    (7 finished) (7 cancelling)))))
+        (trace
+          (sync (poll-evt (thunk
+                            (trace 'polling)
+                            (cond [(zero? count) 'ok]
+                                  [else
+                                    (set! count (sub1 count))
+                                    #f])))))
+        (check-equal? (get)
+                      '((1 polling) (2 polling) (3 polling) (3 ok))))
+
+
+      (test-case "poll-evt with sync/timeout doesn't run polling thread"
+        (define-values (trace get) (make-tracer))
+    
+        (check-false (sync/timeout 0 (poll-evt (thunk
+                                                 (trace 'one)
+                                                 #f))))
+        (check-equal? (get) '((0 one)))
+    
+        (check-eq? (sync/timeout 0 (poll-evt (thunk
+                                               (trace 'two) 
+                                               'ok)))
+                   'ok)
+        (check-equal? (get) '((0 one) (0 two)))))
+
+  (define/provide-test-suite event-tests
+    check-timeout-evt 
+    check-timer-evt 
+    check-poll-evt ))
 
 (module+ test
-  (test-case "poll-evt is ready with the value of  poll when it returns non-false"
-    (define-values (trace get) (make-tracer))
-    (define count 2)
+  (require rackunit/text-ui
+           (submod ".." test-suite))
 
-    (trace
-      (sync (poll-evt (thunk
-                        (trace 'polling)
-                        (cond [(zero? count) 'ok]
-                              [else
-                                (set! count (sub1 count))
-                                #f])))))
-    (check-equal? (get)
-                  '((1 polling) (2 polling) (3 polling) (3 ok)))))
-
-
-(module+ test
-  (test-case "poll-evt with sync/timeout doesn't run polling thread"
-    (define-values (trace get) (make-tracer))
-
-    (check-false (sync/timeout 0 (poll-evt (thunk
-                                             (trace 'one)
-                                             #f))))
-    (check-equal? (get) '((0 one)))
-
-    (check-eq? (sync/timeout 0 (poll-evt (thunk
-                                           (trace 'two) 
-                                           'ok)))
-               'ok)
-    (check-equal? (get) '((0 one) (0 two)))))
-
+  (run-tests event-tests 'verbose))
