79e67a855fbf3c9b2251ecbb89db13ac16747069
Sam Phillips <samdphillips@gmail.com>
2012-07-19 13:46:11 +0000

    More expressiveness in logging syntax

--- logging.rkt
+++ logging.rkt
@@ -2,6 +2,8 @@
 
 (require (for-syntax racket/base
                      syntax/parse
+                     (only-in unstable/sequence
+                              in-syntax)
                      (only-in racket/syntax
                               format-id
                               generate-temporary))
@@ -68,16 +70,32 @@
     (thread serve)
     (values a a.b)))
 
-(define-syntax (define-log-syntax stx)
+(define-syntax (rawlog stx)
+  (syntax-parse stx
+    [(_ logger:id level:id fmt-str fmt-v:expr ... (~seq kw:keyword v) ...)
+     #:with
+     (kvp ...)
+     (for/list ([k (in-syntax #'(kw ...))]
+                [v (in-syntax #'(v  ...))])
+       #`(cons '#,(datum->syntax k (string->symbol (keyword->string (syntax->datum k))))
+               #,v))
+     #`(when (log-level? logger 'level)
+         (let ([message (format fmt-str fmt-v ...)])
+           (log-message logger
+                        'level
+                        message
+                        (log-entry (current-milliseconds)
+                                   (logger-name logger)
+                                   message
+                                   (current-continuation-marks)
+                                   (make-immutable-hash (list kvp ...))))))]))
+
+(define-syntax (define-logging stx)
   (syntax-parse stx
     [(_ log:id level:id logger:id) 
      (syntax/loc stx
        (define-syntax-rule (log fmt-str v (... ...))
-         (when (log-level? logger 'level)
-           (log-message logger 
-                        'level 
-                        (format fmt-str v (... ...))
-                        (vector (current-continuation-marks) #f)))))]))
+         (rawlog logger level fmt-str v (... ...))))]))
 
 (define-syntax (declare-logger stx)
   (syntax-parse stx
@@ -91,58 +109,14 @@
      (for/list ([x '(fatal error warning info debug)])
        (define log (format-id #'logger-name "log-~a" x))
        (define lvl (datum->syntax stx x))
-       #`(define-log-syntax #,log #,lvl logger))
+       #`(define-logging #,log #,lvl logger))
      
      (quasisyntax/loc stx
        (begin
         (define logger (find-or-create-logger! 'logger-name))
         log-stx ...))]))
 
-(define (string-left-trim s)
-  (define i
-    (for/or ([i (in-naturals)]
-             [c (in-string s)])
-      (and (not (char-whitespace? c)) i)))
-  (substring s i))
-
-(module+ test
-  (check string=? (string-left-trim "abc")   "abc")
-  (check string=? (string-left-trim "  abc") "abc")
-  (check string=? (string-left-trim "abc  ") "abc  "))
-
-
 (struct log-entry (timestamp category level message marks extra))
-
-;; vector->log-entry
-;; converts a vector value as produced by synchronizing on a log-receiver into
-;; a log-entry.
-(define (vector->log-entry v)
-  (match-define (vector level msg (vector marks extra)) v)
-  (define i
-    (for/or ([i (in-naturals)]
-             [c (in-string msg)])
-       (and (char=? c #\:) i)))
-  (define category (string->symbol (substring msg 0 i)))
-  (define message  (string-left-trim (substring msg (add1 i))))
-
-  (log-entry (current-milliseconds)
-             category
-             level
-             message
-             marks
-             extra))
-
-(module+ test
-  (test-case "check log-entry from vector"
-    (define e
-      (vector->log-entry (vector 'info "a.b: helloworld" (vector 'c 'd))))
-    (check-eq? (log-entry-level e)    'info)
-    (check-eq? (log-entry-category e) 'a.b)
-    (check string=? (log-entry-message e) "helloworld")
-    (check-eq? (log-entry-marks e) 'c)
-    (check-eq? (log-entry-extra e) 'd)))
-
-
 
 ; which logger and level?
 ; what state to haul around?
@@ -152,6 +126,5 @@
 
 
 (provide find-or-create-logger!
-         define-log-syntax
          declare-logger)
 
